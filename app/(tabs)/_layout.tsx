4b13f42c75ed7746f079106bbe2214c4b1cada12
Rose van Leeuwen | Digital Individuals <rvleeuwen@digitalindividuals.com>
2025-04-05 17:14:51 +0200

    changed layout icon color

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,7 +7,7 @@ export default function TabLayout() {
   return (
     <Tabs
     screenOptions={{
-        tabBarActiveTintColor: '#ffd33d',
+        tabBarActiveTintColor: '#000000',
         headerStyle: {
           // backgroundColor: '#25292e',
         },
